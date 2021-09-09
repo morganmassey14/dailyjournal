f59baccf9624b4ac152e9eb099c13f2d72e86250
tgann11 <morganne1170@gmail.com>
2021-09-09 15:17:37 -0500

    added json

---
+++ src/scripts/DataManager.js
@@ -0,0 +1,20 @@
+// this function gets ALL JOURNAL ENTRIES
+export const getJournalEntries = () => {
+    //this returns back where we invoked it (in main)
+    return fetch("http://localhost:8088/journal-entries")
+    .then(response => response.json())
+}
+
+// this is where this function fetches our data from json and translates it in a way that javascript understands it
+
+export const createJournalEntry = postObj => {
+    return fetch("http://localhost:8088/journal-entries", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(postObj)
+  
+    })
+        .then(response => response.json())
+  }
